1ba4463b4377b5a19bb654320c8d46a631bdf725
Егор Челноков <chelnokov16it18k@yandex.ru>
2021-05-12 15:39:16 +0300

    удалил лишние файлы

--- Startup.cs
+++ Startup.cs
@@ -25,8 +25,6 @@ namespace AddFilesApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationContext>(options =>
-    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<FilesContext>(options =>
     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
